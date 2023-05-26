b000d0c284318324d85d558962fb6b60df3e1096
habitdev <goodstory16@gmail.com>
2023-05-26 14:14:33 +0900

    Add: 일반함수 제너릭

--- udemy/_mine/06_generics/src/app.ts
+++ udemy/_mine/06_generics/src/app.ts
@@ -21,8 +21,39 @@ const mergedObj2 = merge<{ name: string }, { age: number }>({ name: 'Max' }, { a
 // 위와 같이 작성하면 새로운 객체로 합쳐지지 않는다
 // 따라서, 객체의 정확한 구조도 상관하지 않겠다고 입력해야 한다
 // T와 U는 항상 객체여야 한다
-// T와 U의 제네릭 타입 제한 -> 제한하고자 하는 타입 다음에 
+// T와 U의 제네릭 타입 제한 -> 제한하고자 하는 타입 다음에
 // extends를 입력
 // 굳이 제한을 하지 않아도 되는 경우 유연하게 놔둔다
 
 console.log(mergedObj.age);
+
+/**
+ * 다른 일반 함수
+ * 매개변수는 원하는대로 지정해도 되지만 제너릭 타입이어야 한다
+ * (어떤 타입이어도 상관 없기 때문이다)
+ *
+ */
+
+// length가 불명확하다고 나오는 것을 방지하기 위해 명확하게 interface를 사용하여 명시한다
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionTxt = 'Got no value';
+  if (element.length === 1) {
+    // length가 불명확하다고 오류가 난다
+    descriptionTxt = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionTxt = 'Got ' + element.length + ' elements';
+  }
+  return [element, descriptionTxt];
+}
+
+// console.log(countAndDescribe(['Sports', 'Cooking'])); // 동작
+// console.log(countAndDescribe([])); // 동작
+// console.log(countAndDescribe(10)); // 오류 -> 숫자는 length 속성을 지니지 않기 때문이다
+
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+
