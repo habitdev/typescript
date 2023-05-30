74c20e0479ac27e4d2619a3e5653059a01c2aced
habitdev <goodstory16@gmail.com>
2023-05-30 13:54:05 +0900

    Add; 데코레이터 팩토리

--- udemy/_mine/07_decorators/src/app.ts
+++ udemy/_mine/07_decorators/src/app.ts
@@ -11,15 +11,19 @@
  * 데코레이터는 보통 클래스들에 적용된다
  * - 데코레이터는 인수를 받는다
  * - 데코레이터는 실체화되기 전 클래스가 정의만 돼도 실행된다
- * 
+ *
  */
 
 // 클래스 통째에 적용되는 데코레이터
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+function Logger(logstring: string) { 
+  return function (constructor: Function) {
+    // 새 함수를 반환하는 함수로 변경
+    console.log(logstring);
+    console.log(constructor);
+  };
 }
-@Logger // @은 특별한 식별자
+@Logger('LOGGING - PERSON') // @은 특별한 식별자
+// 데코레이터 함수를 실행하려는 게 아니라 데코레이터 함수와 같은 걸 반환해 줄 함수를 실행하는 것
 class Person {
   name = 'Max';
 
@@ -31,3 +35,8 @@ class Person {
 // Person울 실체화하지 않아도 데코레이터(Logger)는 실행된다
 const pers = new Person();
 console.log(pers);
+
+/**
+ * 데코레이터를 만드는 대신, 데코레이터 팩토리(factory)를 정의할 수 있다
+ * 데코레이터 함수를 도출하는데 어떤 대상에 데코레이터를 할당할 때 설정할 수 있도록 해준다
+ */
