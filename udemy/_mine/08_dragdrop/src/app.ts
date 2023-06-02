3a79d6579d381258e30186f640cdae896b8108d2
habitdev <goodstory16@gmail.com>
2023-06-02 11:45:48 +0900

    Add: 폼 입력 완료 시 항목별 입력 값 확인, 폼 비우기

--- udemy/_mine/08_dragdrop/src/app.ts
+++ udemy/_mine/08_dragdrop/src/app.ts
@@ -1,9 +1,26 @@
 // Code goes here!
+// auto bind decorator
+function AutoBind(_: any, _2: string, decorator: PropertyDescriptor) {
+  const originalMethod = decorator.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
 
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// project input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -18,8 +35,53 @@ class ProjectInput {
     // importedNode는 상수(const)라 X
     // importedNode는 DocumentFragment라서 아래의 attach에 삽입할 수 없다
     // 대신 그 안에 있는 확실한 HTML 엘리먼트에 접근해야 한다
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element = importedNode.firstElementChild as HTMLFormElement; // form
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    // 아무것도 반환하지 않는다면 undefined가 아니라 void로 작성하는 것이 좋다
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+      alert('Invalid input, please try again!');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, parseFloat(enteredPeople)];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      // 튜플인지 아닌지는 런타임 중(자바스크립트에선 미지원) 알 수 없으므로
+      // 튜플은 배열이기 때문에 배열인지 검사한다
+
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
