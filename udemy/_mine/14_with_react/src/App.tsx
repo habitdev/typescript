5f8616e76d52eaa4707b686bcd67b379f9556062
habitdev <goodstory16@gmail.com>
2023-06-12 14:39:12 +0900

    Add: 컴포넌트간 커뮤니케이션

--- udemy/_mine/14_with_react/src/App.tsx
+++ udemy/_mine/14_with_react/src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [{ id: 'todo1', text: 'Finish the course!' }];
 
-  return <div className='App'>
-    {/* A component that adds todos */}
-    {/* prop의 이름은 마음대로 정할 수 있다 => items */}
-    <TodoList items={todos}/>
-  </div>;
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
+  return (
+    <div className='App'>
+      {/* A component that adds todos */}
+      {/* prop의 이름은 마음대로 정할 수 있다 => items */}
+      <NewTodo onAddTodo={todoAddHandler} />
+      {/* onAddTodo이란 prop이 todoAddHandler을 전달 */}
+      <TodoList items={todos} />
+    </div>
+  );
 }
 
 export default App;
