258f6d9cc731e294b9be1d7c249d41cf69c962e1
habitdev <goodstory16@gmail.com>
2023-06-12 15:02:54 +0900

    Add: useState로 상태관리하기

--- udemy/_mine/14_with_react/src/App.tsx
+++ udemy/_mine/14_with_react/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from "./todo.model";
 
 function App() {
-  const todos = [{ id: 'todo1', text: 'Finish the course!' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // useState은 항상 2가지를 리턴한다
+  // 첫번째는 render 사이클에 가장 최근 state 스냅샷
+  // 두번째는 state를 업데이트 하는 function이고 컴포넌트를 다시 렌더링한다
+  // useState는 초기화 시 값이 비면 오류가 나므로 어떤 데이터가 들어올 지 설정해준다
+  // useState에 배열을 담아 보내는데 형식은 Todo와 같다
 
   const todoAddHandler = (text: string) => {
     console.log(text);
+    setTodos(prevTodos => [...prevTodos ,{ id: Math.random().toString(), text: text }]);
+    // 가장 최신의 모습에 배열의 정보를 담는다
   };
 
   return (
