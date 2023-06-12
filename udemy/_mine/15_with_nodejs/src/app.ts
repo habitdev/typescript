444dedd8dd9545526592281aec0be3a3dbef9df9
habitdev <goodstory16@gmail.com>
2023-06-12 15:55:42 +0900

    Add: express, body-parser, nodemon 설치

---
+++ udemy/_mine/15_with_nodejs/src/app.ts
@@ -0,0 +1,11 @@
+console.log('Something...');
+
+/**
+ * NodeJS는 파일 형식과 상관없이 내용이 자바스크립트라면 실행한다
+ * 하지만, 타입스크립트는 실행하지 않는다
+ *
+ * "body-parser": "^1.20.2", // 들어오는 request body를 전달할 수 있다
+ * "express": "^4.18.2" // node express
+ * "nodemon": "^1.19.4" // 노드 변화 시마다 서버를 재시작
+ * 
+ */
